cd8d5acf3c5be1f8a8738cc5d47f60900db2326e
kushthakker <kushthakker777@gmail.com>
2021-07-09 19:15:14 +0530

    New feature to new branch

--- src/js/search.js
+++ src/js/search.js
@@ -450,6 +450,7 @@ export const search = function (e, main) {
     document.querySelector(".modal-search").classList.add("hidden");
     document.querySelector(".modal-bookmark").classList.remove("hidden");
     document.querySelector(".modal-addRecipe").classList.remove("hidden");
+    console.log(`hola new branch`);
   } catch (err) {
     alert(err.message);
   }
